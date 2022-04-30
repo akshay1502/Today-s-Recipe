cff9f16c264c1af7b3126ad954bd2ad392438882
akshay shinde <akshayshinde7289@gmail.com>
2022-04-30 17:24:02 +0530

    C:/Users/Akshay/AppData/Local/Programs/Git/addRecipe page ui donw

--- notes.md
+++ notes.md
@@ -3,7 +3,20 @@ use autogrow for increasing the height of the ckeditor dynamically.
 Will use editor.js has better YT video support, and clean documentation. Uninstall draft.js
 
 next goal
-1. complete addRecipe page
+1. complete addRecipe page (done)
 2. Start working for showing of recipes
 3. signin/ signup form
 4. work on backend - authentication(JWT token)
+
+
+Known problems
+- editorJs creating two instances of editor for single rendering (instance creation in useEffect)
+
+
+Learnings
+- learn sass
+- features of editor js
+- depth concepts of react js
+- authentication (JWT)
+
+https://github.com/editor-js/awesome-editorjs#react (use react editorJS editor, best features and easy implementation)
--- src/components/addRecipe/addRecipe.js
+++ src/components/addRecipe/addRecipe.js
@@ -1,16 +1,91 @@
-import React from 'react';
-import MyEditor from '../editor/editor';
+import React, { useEffect } from 'react';
 import './addRecipe.scss';
+import EditorJS from '@editorjs/editorjs';
+import Header from '@editorjs/header';
+import List from '@editorjs/list';
+import NestedList from '@editorjs/nested-list';
+import Embed from '@editorjs/embed';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import Checklist from '@editorjs/checklist';
 
 export default function AddRecipe() {
+  let recipeEditor;
+  let ingredientListEditor;
+  let recipeTitleEditor;
+  useEffect(() => {
+    recipeTitleEditor = new EditorJS({
+      holder: 'recipeTitle',
+      placeholder: 'Recipe Title...',
+      defaultBlock: 'paragraph',
+    });
+
+    recipeEditor = new EditorJS({
+      holder: 'recipe',
+      placeholder: 'Enter recipe here...',
+      tools: {
+        header: {
+          class: Header,
+          config: {
+            placeholder: 'Enter a heading',
+            levels: [1, 2, 3],
+            defaultLevel: 2,
+          },
+        },
+        list: {
+          class: NestedList,
+          inlineToolbar: true,
+        },
+        embed: {
+          class: Embed,
+          config: {
+            services: {
+              youtube: true,
+              facebook: true,
+              instagram: true,
+              twitter: true,
+            },
+          },
+          inlineToolbar: true,
+        },
+        Checklist: {
+          class: Checklist,
+          inlineToolbar: true,
+        },
+      },
+    });
+
+    ingredientListEditor = new EditorJS({
+      holder: 'ingredientList',
+      placeholder: "Ingredient List (use 'List' tool)",
+      tools: {
+        list: {
+          class: List,
+          inlineToolbar: true,
+          config: {
+            defaultStyle: 'unordered',
+          },
+        },
+      },
+    });
+  }, []);
+  const storeRecipeData = () => {
+    recipeTitleEditor.save().then((data) => console.log(data));
+    recipeEditor.save().then((data) => console.log(data));
+    ingredientListEditor.save().then((data) => console.log(data));
+  };
+
   return (
     <div className="cook">
-      <input type="text" name="recipeTitle" id="recipeTitle" placeholder="Recipe Title..." />
+      <div id="recipeTitle" />
       <div className="InputImageContainer">
         <button type="button" onClick={() => document.getElementById('inputImage').click()}>Upload Recipe Image</button>
         <input type="file" style={{ display: 'none' }} id="inputImage" />
       </div>
-      <MyEditor />
+      <div className="recipeData">
+        <div id="ingredientList" />
+        <div id="recipe" />
+      </div>
+      <button type="button" onClick={storeRecipeData} id="postRecipe">Post recipe</button>
     </div>
   );
 }
